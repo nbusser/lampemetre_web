815624f099f1045083a847373871b44658207524
Nicolas BUSSER <nicolas.busser67@gmail.com>
2021-07-24 12:41:34 +0200

    add vertical bar when clicking on a point

--- lampemetre.js
+++ lampemetre.js
@@ -30,7 +30,52 @@ var chart = new Chart("chart", {
   data: {
     labels: xValues,
     datasets: [],
-}});
+  },
+  options: {
+    onClick: (e) => measure(chart, e),
+    annotation: {
+      annotations: []
+    },
+  },
+});
 
 add_line_chart(chart, "rgba(0,0,255,1.0)", yValues);
 add_line_chart(chart, "rgba(255,0,0,1.0)", yValues2);
+
+var measure = function(chart, e) {
+  // Checks if user is hovering a valid sample point
+  if(chart.tooltip._active.length) {
+    const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
+    const index_x = chart.scales['x-axis-0'].getValueForPixel(canvasPosition.x);
+
+    addAnnotationVertical(xValues[index_x], "test")
+  }
+}
+
+function addAnnotationVertical(xValue, text){
+	var line = 'line' + xValue;
+	chart.options.annotation.annotations.push(
+		{
+			drawTime: "afterDatasetsDraw",
+			id: line,
+			type: "line",
+			mode: "vertical",
+			scaleID: "x-axis-0",
+			value: xValue,
+			borderColor: "black",
+			borderWidth: 3,
+			label:
+			{
+				fontColor: "black",
+				backgroundColor: "white",
+				content: text,
+				enabled: true
+			}
+		});
+    chart.update();
+}
+
+function clear_annotations() {
+  chart.options.annotation.annotations = [];
+  chart.update();
+}
