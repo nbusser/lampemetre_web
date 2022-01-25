0698424e1a850bf592ec73a694bc6811c61c6618
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-01-25 17:52:50 +0100

    improve timer's documentation

--- src/ts/Timer.ts
+++ src/ts/Timer.ts
@@ -1,3 +1,8 @@
+/*
+This class acts as a simple timer 'widget'
+The user can use it to know if his lamps are hot enough
+It also allows an option to block any capture before it reached 0
+*/
 export default class Timer {
   private timerText: HTMLParagraphElement;
 
@@ -30,7 +35,9 @@ export default class Timer {
     this.updateTimer(false);
   }
 
+  // Called every second and when initializing Timer class
   private updateTimer(decrement: boolean) {
+    // Prints the timer value on two digits
     this.timerText.textContent = this.secondsLeft.toLocaleString('fr', {
       minimumIntegerDigits: 2,
       useGrouping: false,
@@ -55,6 +62,8 @@ export default class Timer {
     return this.isBlockingCheckbox.checked;
   }
 
+  // Called by ViewTube when '+' button is clicked
+  // If the timer is not 0 and the user checked the checkbox, we pop an alert window and return true
   public blockIfNecessary(): boolean {
     if (this.shouldBlock() && !this.isOver()) {
       alert('Les lampes de sont pas encore chaudes. Veuillez attendre la fin du minuteur');
